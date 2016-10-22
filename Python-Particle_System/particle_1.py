343ef3568b5eeab7fdb5b79e7deb1b411e0480f0
SkyOnUnix <skylar.chutka@sbcglobal.net>
2016-10-21 23:13:02 -0700

    Work on drawing pixel

---
+++ Python-Particle_System/particle_1.py
@@ -0,0 +1,14 @@
+import math
+import kivy
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.graphics import *
+
+
+class particle(App):
+    def build(self):
+        
+
+if __name__ == '__main__':
+    particle().run()
