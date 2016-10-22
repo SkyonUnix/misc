47aa8df5dcb32830299d0845563f71b2d8e9480e
SkyonUnix <ultimateidiot25@gmail.com>
2016-10-22 12:03:09 -0700

    Testing draw techniques

--- Python-Particle_System/particle_1.py
+++ Python-Particle_System/particle_1.py
@@ -2,10 +2,8 @@ import math
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.graphics import *
 
 class pixel(Widget):
-    pass
 
 class particle(App):
     def build(self):
